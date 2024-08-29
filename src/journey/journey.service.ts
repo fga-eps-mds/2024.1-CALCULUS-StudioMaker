f6ba2ffa6e8c5614abecc6390473493d462ed506
DaviMatheus <matdavimat@gmail.com>
2024-08-29 15:00:16 -0300

    [feat]#79-refatoring_journey_logic
    Co-authored-by: natangoatoso  <Natan.almeida03@gmail.com>
    Co-authored-by: Neoprot  <kaua.seichi.gomes@gmail.com>
    Co-authored-by: Joa0V  <joao.vitortnt5@gmail.com>

--- src/journey/journey.service.ts
+++ src/journey/journey.service.ts
@@ -1,11 +1,5 @@
 import { HttpService } from '@nestjs/axios';
-import { firstValueFrom } from 'rxjs';
-import {
-  Injectable,
-  Logger,
-  NotFoundException,
-  UnauthorizedException,
-} from '@nestjs/common';
+import { Injectable, Logger, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model, Types } from 'mongoose';
 import { Journey } from './journey.schema';
@@ -27,29 +21,18 @@ export class JourneyService {
 
   async create(
     createJourneyDto: CreateJourneyDto,
-    token: string,
     pointId: string,
   ): Promise<Journey> {
-    const userId = await this.pointService.validateTokenAndGetUserId(token);
-
     const pointExist = await this.pointModel.findById(pointId).exec();
     if (!pointExist) {
       throw new NotFoundException(`Point with ID ${pointId} not found`);
-    }
-    this.logger.log(`User ID from token: ${userId}`);
-
-    if (!userId) {
-      throw new UnauthorizedException('Invalid token');
     }
 
     const newJourney = new this.journeyModel({
       ...createJourneyDto,
       point: pointId,
-      user: userId,
     });
     const savedJourney = await newJourney.save();
-
-    await this.addJourneyToUser(userId, savedJourney._id.toString());
 
     await this.pointService.addJourneyToPoint(
       pointId,
@@ -59,27 +42,12 @@ export class JourneyService {
     return savedJourney;
   }
 
-  async addJourneyToUser(userId: string, journeyId: string): Promise<void> {
-    try {
-      await firstValueFrom(
-        this.httpService.patch(
-          `${process.env.USER_SERVICE_URL}/${userId}/add-journey`,
-          { journeyId },
-        ),
-      );
-      this.logger.log(`Added journey ${journeyId} to user ${userId}`);
-    } catch (err) {
-      this.logger.error(`Failed to add journey to user: ${err.message}`);
-      throw new NotFoundException('Failed to update user with new journey');
-    }
-  }
-
   async findAll(): Promise<Journey[]> {
     return this.journeyModel.find().exec();
   }
 
-  async findByUserId(userId: string): Promise<Journey[]> {
-    return this.journeyModel.find({ user: userId }).exec();
+  async findByPointId(pointId: string): Promise<Journey[]> {
+    return this.journeyModel.find({ point: pointId }).exec();
   }
 
   async findById(id: string): Promise<Journey> {
